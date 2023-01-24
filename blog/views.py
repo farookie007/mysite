e7423135ba7f49247fdade0520a971036b76e397
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-01-24 11:53:53 +0100

    Added pagination to the `post_list` view and its exceptions

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get("page", 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # if the page requested is not an integer page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if `page_number` is out of range, it renders the last page
+        posts = paginator.page(paginator.num_pages)
+
     return render(request, "blog/post/list.html", context={"posts": posts})
 
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
     return render(request, "blog/post/detail.html", context={"post": post})
