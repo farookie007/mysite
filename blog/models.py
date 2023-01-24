f8e432e5409ce3bbc66f7170407ba61064b56d95
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-01-24 11:51:47 +0100

    Modified the Post model to return the correct absolute url

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,7 @@
-from random import choices
-from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -39,7 +38,7 @@ class Post(models.Model):
         PUBLISHED = ("PB", "Published")
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
@@ -57,3 +56,15 @@ class Post(models.Model):
     def __str__(self):
         """String representation for the Post model."""
         return self.title
+
+    def get_absolute_url(self):
+        """Returns the url for a particular post using its id"""
+        return reverse(
+            "blog:post_detail",
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug,
+            ],
+        )
