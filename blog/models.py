7a2cda7f70fe0536106259d18a0f309848940a65
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-01-30 10:48:32 +0100

    Created the `Comment` model and added the tag field for the `Post` model

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -28,7 +29,9 @@ class Post(models.Model):
     """
 
     class Meta:
-        ordering = ["-publish"]
+        ordering = [
+            "-publish",
+        ]
         indexes = [
             models.Index(fields=["-publish"]),
         ]
@@ -40,25 +43,31 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date="publish")
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
+        User,
+        on_delete=models.CASCADE,
+        related_name="blog_posts",
     )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(
-        max_length=2, choices=Status.choices, default=Status.DRAFT
+        max_length=2,
+        choices=Status.choices,
+        default=Status.DRAFT,
     )
     objects = models.Manager()  # the default manager
     published = PublishedManager()  # our custom manager
     draft = DraftManager()  # for drafted posts
+    tags = TaggableManager()  # for tags
 
     def __str__(self):
         """String representation for the Post model."""
         return self.title
 
     def get_absolute_url(self):
-        """Returns the url for a particular post using its id"""
+        """Returns the absolute url for a particular post using its
+        url parameters"""
         return reverse(
             "blog:post_detail",
             args=[
@@ -68,3 +77,35 @@ class Post(models.Model):
                 self.slug,
             ],
         )
+
+
+class Comment(models.Model):
+    """
+    This model represents how the comment by each user is stored
+    in the database.
+    """
+
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    objects = models.Manager()
+
+    class Meta:
+        ordering = [
+            "-created",
+        ]
+        indexes = [
+            models.Index(fields=["-created"]),
+        ]
+
+    def __str__(self):
+        """String representation for the Comment model."""
+        return f"Comment by {self.name} on {self.post}"
